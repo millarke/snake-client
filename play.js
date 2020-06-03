8b77dc89403d59ac16bbe7f69d2e95082326f6f4
Keith Millar <keithmillar@hotmail.ca>
2020-06-03 18:20:56 +0000

    split the connect function into a sep client file and required it for the play file

--- play.js
+++ play.js
@@ -1,23 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.148',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  // is this what lets me recieve msgs from the server?
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
